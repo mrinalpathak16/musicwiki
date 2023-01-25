71e9af2ee3e68c58ba8274dcbe84e51d058bb56a
Mrinal Pathak <pathak.mrinal2000@rediffmail.com>
2023-01-25 16:55:33 +0530

    Layouts and NavGraph added.

---
+++ app/src/main/java/com/pathakbau/musicwiki/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pathakbau.musicwiki.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pathakbau.musicwiki.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/pathakbau/musicwiki/ui/fragments/AlbumDetailsFragment.kt
@@ -0,0 +1,6 @@
+package com.pathakbau.musicwiki.ui.fragments
+
+import androidx.fragment.app.Fragment
+
+class AlbumDetailsFragment: Fragment() {
+}
